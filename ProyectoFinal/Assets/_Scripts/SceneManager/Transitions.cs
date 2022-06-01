1a335e7552266be9727e2f6242a92348d00cd5d8
GalenKishi <51836206+GalenKishi@users.noreply.github.com>
2022-05-31 20:15:22 -0500

    Transition V.1

--- ProyectoFinal/Assets/_Scripts/SceneManager/Transitions.cs
+++ ProyectoFinal/Assets/_Scripts/SceneManager/Transitions.cs
@@ -1,14 +1,13 @@
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
 
 public class Transitions : MonoBehaviour
 {
     [SerializeField] GameObject _creditsGameObject;
-
-    public void Play()
-    {
-        SceneManager.LoadScene("Game");
-    }
+    [SerializeField] GameObject _activateTransitionPlay;
+    [SerializeField] Slider _slider;
 
     public void Credits()
     {
@@ -23,5 +22,29 @@ public class Transitions : MonoBehaviour
     public void Exit()
     {
         Application.Quit();
+    }
+
+    public void LoadLevel(int _sceneIndex)
+    {
+        _activateTransitionPlay.SetActive(true);
+        StartCoroutine(WaitForPlay(_sceneIndex));
+    }
+
+    private IEnumerator WaitForPlay(int _sceneIndex)
+    {
+        yield return new WaitForSeconds(3f);
+        StartCoroutine(LoadAsynchonously(_sceneIndex));
+    }
+
+    private IEnumerator LoadAsynchonously(int _sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(_sceneIndex);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            _slider.value = Mathf.Lerp(_slider.minValue, _slider.maxValue, Time.deltaTime * 0.9f);
+            yield return null;
+        }
     }
 }
